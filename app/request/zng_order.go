1124e78c940c26264188dbfeee64d8a5794e09b2
zngue <zngfue@163.com>
2021-06-25 14:27:33 +0800

    1

--- app/request/zng_order.go
+++ app/request/zng_order.go
@@ -1,13 +1,21 @@
 package request
+
 import (
 	"github.com/zngue/go_helper/pkg"
 	"gorm.io/gorm"
 )
+
 type ZngOrderRequest struct {
 	pkg.CommonRequest
-	ID int `form:"id" field:"id" where:"eq" default:"0"`
+	ID     int    `form:"id" field:"id" where:"eq" default:"0"`
+	OpenID string `form:"openid" field:"openid" where:"eq" default:""`
 }
+
 func (r *ZngOrderRequest) Common(db *gorm.DB) *gorm.DB {
+	r.OrderMap = map[string]interface{}{
+		"1": "id desc",
+	}
+	r.OrderString = "1"
 	tx := r.Init(db, *r)
 	return tx
 }
