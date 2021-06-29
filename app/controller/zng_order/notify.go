82063cb88e1c197551fb1412743772f98b65b0f8
zngue <zngfue@163.com>
2021-06-29 15:00:24 +0800

    1

--- app/controller/zng_order/notify.go
+++ app/controller/zng_order/notify.go
@@ -3,9 +3,13 @@ package zng_order
 import (
 	"time"
 
+	"github.com/zngue/carmichael/app/request"
+	"github.com/zngue/carmichael/app/service"
+
 	"github.com/gin-gonic/gin"
 	"github.com/zngue/carmichael/app/model"
 	"github.com/zngue/go_helper/pkg"
+	"golang.org/x/sync/errgroup"
 )
 
 type Response struct {
@@ -26,24 +30,97 @@ func Notify(ctx *gin.Context) {
 		})
 		return
 	}
-	err := pkg.MysqlConn.Model(&model.ZngOrder{}).Where("order_num = ?", rep.MchIdNo).Updates(map[string]interface{}{
-		"pay_status":  1,
-		"update_time": time.Now().Unix(),
-		"openid":      rep.Openid,
-	}).Error
+	begin := pkg.MysqlConn.Begin()
+	var shopReq request.ZngOrderRequest
+	shopReq.OrderNum = rep.MchIdNo
+	detail, err := service.NewZngOrderService().Detail(&shopReq)
+	if err != nil || detail == nil {
+		ctx.JSON(500, gin.H{
+			"code":    100,
+			"message": "更新失败",
+		})
+		return
+	}
+	var kmOne model.ZngKm
+	var shopOne model.ZngShop
 
+	var shopwg errgroup.Group
+	shopwg.Go(func() error { //获取卡密
+		return pkg.MysqlConn.Model(&model.ZngKm{}).Where("status = ?", 0).Where(" shop_id =  ? ", detail.ShopId).First(&kmOne).Error
+	})
+	shopwg.Go(func() error { // 获取商品信息
+		return pkg.MysqlConn.Model(&model.ZngShop{}).Where("id = ?", detail.ShopId).First(&shopOne).Error
+	})
+	if err := shopwg.Wait(); err != nil {
+		ctx.JSON(500, gin.H{
+			"code":    100,
+			"message": "更新失败",
+		})
+		return
+	}
+	if &kmOne == nil || &shopOne == nil {
+		ctx.JSON(500, gin.H{
+			"code":    100,
+			"message": "更新失败",
+		})
+		return
+	}
+	defer begin.Rollback()
+	var wg errgroup.Group
+	wg.Go(func() error { //更新订单信息
+		return begin.Model(&model.ZngOrder{}).Where("order_num = ?", rep.MchIdNo).Updates(map[string]interface{}{
+			"pay_status":  1,
+			"update_time": time.Now().Unix(),
+			"openid":      rep.Openid,
+		}).Error
+
+	})
+	if shopOne.CateId == 1 { // idea 账号密码不更新数据库
+		wg.Go(func() error {
+			return begin.Model(&model.ZngUser{}).Where("openid = ?", rep.Openid).Updates(map[string]interface{}{
+				"account":      kmOne.Account,
+				"password":     kmOne.Password,
+				"ext_account":  kmOne.ExtAccount,
+				"ext_password": kmOne.ExtPassword,
+				"expire_time":  time.Now().Unix() + kmOne.ExpireTime,
+			}).Error
+		})
+	} else {
+		wg.Go(func() error {
+			return begin.Model(&model.ZngUser{}).Where("openid = ?", rep.Openid).Updates(map[string]interface{}{
+				"account":     kmOne.Account,
+				"password":    kmOne.Password,
+				"expire_time": time.Now().Unix() + kmOne.ExpireTime,
+			}).Error
+		})
+		wg.Go(func() error {
+			return begin.Model(&model.ZngKm{}).Where("id = ?", kmOne.Id).Updates(map[string]interface{}{
+				"Status": 1,
+			}).Error
+		})
+
+	}
+	err = wg.Wait()
 	if err != nil {
 		ctx.JSON(500, gin.H{
 			"code":    100,
 			"message": "更新失败",
 		})
 		return
-	} else {
-		ctx.JSON(200, gin.H{
-			"code":    200,
-			"message": "支付成功",
+	}
+	errdb := begin.Commit().Error
+	if errdb != nil {
+		ctx.JSON(500, gin.H{
+			"code":    100,
+			"message": "更新失败",
 		})
 		return
 	}
+
+	ctx.JSON(200, gin.H{
+		"code":    200,
+		"message": "支付成功",
+	})
+	return
 
 }
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/spf13/viper v1.7.1
 	github.com/xinliangnote/go-util v0.0.0-20200323134426-527984dc34bf
 	github.com/zngue/go_helper v0.0.0-20210602011314-f4f0160c3d63
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.0.5 // indirect
 	gorm.io/gorm v1.21.3
