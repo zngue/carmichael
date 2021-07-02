4153632eddc5d3dd7da1f14f1945d958d9e49410
zngue <zngfue@163.com>
2021-07-02 15:07:24 +0800

    1

--- app/controller/zng_shop/list.go
+++ app/controller/zng_shop/list.go
@@ -6,13 +6,17 @@ import (
 	"github.com/zngue/carmichael/app/service"
 	"github.com/zngue/go_helper/pkg/response"
 )
-func  List(ctx *gin.Context) {
+
+func List(ctx *gin.Context) {
 	var req request.ZngShopRequest
 	if err := ctx.ShouldBind(&req); err != nil {
-		response.HttpParameterError(ctx,err)
+		response.HttpParameterError(ctx, err)
 		return
 	}
+	if req.Status == 0 {
+		req.Status = 1
+	}
 	res, err := service.NewZngShopService().List(&req)
-	response.HttpSuccessWithError(ctx,err,res)
+	response.HttpSuccessWithError(ctx, err, res)
 	return
 }
--- app/request/zng_shop.go
+++ app/request/zng_shop.go
@@ -1,12 +1,16 @@
 package request
+
 import (
 	"github.com/zngue/go_helper/pkg"
 	"gorm.io/gorm"
 )
+
 type ZngShopRequest struct {
 	pkg.CommonRequest
-	ID int `form:"id" field:"id" where:"eq" default:"0"`
+	ID     int `form:"id" field:"id" where:"eq" default:"0"`
+	Status int `form:"status" field:"status" where:"eq" default:"0"`
 }
+
 func (r *ZngShopRequest) Common(db *gorm.DB) *gorm.DB {
 	tx := r.Init(db, *r)
 	return tx
